d8220b36e78be4f6ecb26556addeb5bcb5a42899
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-19 22:39:28 -0300

    ok

---
+++ dist/commands/server/server-discovery.js
@@ -0,0 +1,50 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const oceanic_js_1 = require("oceanic.js");
+const command_1 = __importDefault(require("../../app/command"));
+exports.default = new command_1.default()
+    .addName("server discovery")
+    .setRun(async ({ app, guild, interaction }) => {
+    if (!guild)
+        return;
+    const discovery = guild.discoverySplashURL();
+    if (!discovery) {
+        interaction.createFollowup({
+            flags: oceanic_js_1.MessageFlags.IS_COMPONENTS_V2,
+            components: [{
+                    type: 10,
+                    content: `${await app.getMenoji("no")} Este servidor não possue fundo de discovery!`,
+                }],
+        });
+        return;
+    }
+    interaction.createFollowup({
+        flags: oceanic_js_1.MessageFlags.IS_COMPONENTS_V2,
+        components: [{
+                type: 17,
+                components: [{
+                        type: 10,
+                        content: `**${guild.name}**`,
+                    }, {
+                        type: 12,
+                        items: [{
+                                media: {
+                                    url: discovery,
+                                },
+                            }],
+                    }],
+            }, {
+                type: 1,
+                components: [{
+                        type: 2,
+                        style: 5,
+                        url: discovery,
+                        label: "Baixar",
+                        emoji: await app.getButoji("download"),
+                    }]
+            }]
+    });
+});
