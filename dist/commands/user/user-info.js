7fabaa2c15221d360e14ea138e42e58c320c2cca
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-20 14:39:24 -0300

    ok

--- dist/commands/user/user-info.js
+++ dist/commands/user/user-info.js
@@ -16,6 +16,10 @@ exports.default = new command_1.default()
     const joinedAt = member && member.joinedAt ?
         parseInt(`${member.joinedAt.getTime() / 1000}`) :
         undefined;
+    const avatar = user.avatarURL();
+    const banner = user.bannerURL();
+    const avatarLocal = member?.avatarURL();
+    const bannerLocal = member?.bannerURL();
     const bot = user.bot ? await (0, getApp_1.default)(user.id) : undefined;
     let content = [
         `**${user.globalName ?? user.username}**\n`,
@@ -41,9 +45,51 @@ exports.default = new command_1.default()
         components: [{
                 type: 17,
                 components: [{
-                        type: 10,
-                        content: content.filter((c) => c !== undefined).join("\n")
+                        type: 9,
+                        components: [{
+                                type: 10,
+                                content: content.filter((c) => c !== undefined).join("\n")
+                            }],
+                        accessory: {
+                            type: 11,
+                            media: {
+                                url: avatar,
+                            },
+                        },
                     }]
+            }, {
+                type: 1,
+                components: [{
+                        type: 2,
+                        style: 5,
+                        label: "Perfil",
+                        emoji: await app.getButoji("link"),
+                        url: `https://discord.com/users/${user.id}`,
+                    }, {
+                        type: 2,
+                        style: 5,
+                        url: avatar,
+                        label: "Avatar Global",
+                        emoji: await app.getButoji("download"),
+                    }, banner ? {
+                        type: 2,
+                        style: 5,
+                        url: avatar,
+                        label: "Estandarte Global",
+                        emoji: await app.getButoji("download"),
+                    } : undefined, member && avatarLocal && avatarLocal !== avatar ? {
+                        type: 2,
+                        style: 5,
+                        url: avatarLocal,
+                        label: "Avatar Local",
+                        emoji: await app.getButoji("download"),
+                    } : undefined, member && bannerLocal && bannerLocal !== banner ? {
+                        type: 2,
+                        style: 5,
+                        url: bannerLocal,
+                        label: "Estandarte Local",
+                        emoji: await app.getButoji("download"),
+                    } : undefined].filter((c) => c !== undefined),
             }],
     });
 });
