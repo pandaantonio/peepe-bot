f0ad8d5aa3e085160083bb5a863dad6ae5749c68
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-20 14:37:58 -0300

    ok

--- src/commands/user/user-info.ts
+++ src/commands/user/user-info.ts
@@ -1,4 +1,4 @@
-import { MessageFlags } from "oceanic.js";
+import { MessageActionRowComponent, MessageFlags } from "oceanic.js";
 import Command from "@/struct/command";
 import getApp from "@/utils/getApp";
 
@@ -14,6 +14,12 @@ export default new Command()
         const joinedAt = member && member.joinedAt ?
             parseInt(`${member.joinedAt.getTime() / 1000}`) :
             undefined;
+
+        const avatar = user.avatarURL();
+        const banner = user.bannerURL();
+
+        const avatarLocal = member?.avatarURL();
+        const bannerLocal = member?.bannerURL();
 
         const bot = user.bot ? await getApp(user.id) : undefined;
 
@@ -42,9 +48,51 @@ export default new Command()
             components: [{
                 type: 17,
                 components: [{
-                    type: 10,
-                    content: content.filter((c) => c !== undefined).join("\n")
+                    type: 9,
+                    components: [{
+                        type: 10,
+                        content: content.filter((c) => c !== undefined).join("\n")
+                    }],
+                    accessory: {
+                        type: 11,
+                        media: {
+                            url: avatar,
+                        },
+                    },
                 }]
+            }, {
+                type: 1,
+                components: [{
+                    type: 2,
+                    style: 5,
+                    label: "Perfil",
+                    emoji: await app.getButoji("link"),
+                    url: `https://discord.com/users/${user.id}`,
+                }, {
+                    type: 2,
+                    style: 5,
+                    url: avatar,
+                    label: "Avatar Global",
+                    emoji: await app.getButoji("download"),
+                }, banner ? {
+                    type: 2,
+                    style: 5,
+                    url: avatar,
+                    label: "Estandarte Global",
+                    emoji: await app.getButoji("download"),
+                }   : undefined, member && avatarLocal && avatarLocal !== avatar ? {
+                    type: 2,
+                    style: 5,
+                    url: avatarLocal,
+                    label: "Avatar Local",
+                    emoji: await app.getButoji("download"),
+                }: undefined, member && bannerLocal && bannerLocal !== banner ? {
+                    type: 2,
+                    style: 5,
+                    url: bannerLocal,
+                    label: "Estandarte Local",
+                    emoji: await app.getButoji("download"),
+                } : undefined].filter((c) => c !== undefined),
             }],
         });
     });
