aba69e19bd1bcd1901213be30bf2a316982f4d79
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-19 23:02:31 -0300

    ok

--- dist/events/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const event_1 = __importDefault(require("../app/event"));
-exports.default = new event_1.default("on", "error", async (app, error) => {
-    console.log(error);
-});
