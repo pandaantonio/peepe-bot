79da97ecadbdf3f6232e3ae115320b746b5203b4
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-25 16:51:53 -0300

    ok

---
+++ dist/commands/dashboard.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const command_1 = __importDefault(require("@/struct/command"));
+const oceanic_js_1 = require("oceanic.js");
+exports.default = new command_1.default()
+    .setCommand({
+    type: 1,
+    name: "dashboard",
+    nameLocalizations: {
+        "pt-BR": "painel",
+    },
+    description: "Access the bot's control panel.",
+    descriptionLocalizations: {
+        "pt-BR": "Acesse o painel de controle do bot.",
+    },
+})
+    .setRun(async function ({ app, guild, interaction }) {
+    const dashboard = guild ? `${process.env.WEBSITE}/dashboard/${guild.id}` : `${process.env.WEBSITE}/dashboard`;
+    interaction.createFollowup({
+        flags: oceanic_js_1.MessageFlags.IS_COMPONENTS_V2,
+        components: [{
+                type: 10,
+                content: dashboard,
+            }, {
+                type: 1,
+                components: [{
+                        type: 2,
+                        style: 5,
+                        url: dashboard,
+                        emoji: await app.getButoji("config"),
+                    }],
+            }]
+    });
+});
