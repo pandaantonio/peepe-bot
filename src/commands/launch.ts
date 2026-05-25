59e3704c3e53aefdbd48b362d9ca1b97c7adf66d
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-24 21:29:03 -0300

    ok

---
+++ src/commands/launch.ts
@@ -0,0 +1,11 @@
+import Command from "@/struct/command";
+
+export default new Command().setCommand({
+    type: 4, // PRIMARY_ENTRY_POINT
+    name: "launch",
+    nameLocalizations: {
+        "pt-BR": "Abrir atividade",
+    },
+    handler: 2,
+    dmPermission: false,
+});
