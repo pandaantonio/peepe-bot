5d1efe4e4a239b82a54f10c8f710358766d936ec
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-25 15:36:14 -0300

    ok

--- src/commands/games/PlayCommand.ts
+++ src/commands/games/PlayCommand.ts
@@ -23,20 +23,7 @@ export default new Command().setCommand({
         descriptionLocalizations: {
             "pt-BR": "Tente adivinhar o número secreto"
         },
-        options: [{
-            minValue: 1,
-            maxValue: 100,
-            required: true,
-            name: "chosen-number",
-            nameLocalizations: {
-                "pt-BR": "número-escolhido"
-            },
-            description: "Choose a number from 1 to 100.",
-            descriptionLocalizations: {
-                "pt-BR": "Escolha um número de 1 a 100."
-            },
-            type: ApplicationCommandOptionTypes.INTEGER,
-        }, EphemeralOption(false)],
+        options: [EphemeralOption(false)],
         type: ApplicationCommandOptionTypes.SUB_COMMAND,
     }, {
         name: "2048",
