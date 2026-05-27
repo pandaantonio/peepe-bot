842defcf0c73f4e83fe5403cadd0c379a359aa1f
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-26 22:03:42 -0300

    ok

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,9 +1,21 @@
 import { adminDb } from "@/database/firebaseAdmin";
 import Event from "@/struct/event";
+import axios from "axios";
+
+interface AutoroleData {
+    guildId: string;
+    users: string[];
+    apps: string[];
+};
+
+async function getData(id: string): Promise<AutoroleData | undefined> {
+    return await axios.get(`${process.env.WEBSITE}/api/guild/${id}/autorole`)
+        .then((res) => res.data)
+        .catch(() => undefined);
+}
 
 export default new Event("on", "guildMemberAdd", async (app, member) => {
-    const snapshot = await adminDb.ref(`autorole/${member.guildID}`).once('value');
-    const data: { guildID: string, users: string[], apps: string[] } | undefined = snapshot.val();
+    const data = await getData(member.guildID)
 
     if(data){
         if(data.users && data.users[0] && !member.bot){
