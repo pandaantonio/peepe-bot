4728353d7572409a34833c5fd91e3629db3f0d13
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-05-28 13:10:59 -0300

    ok

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,6 +1,7 @@
 import { adminDb } from "@/database/firebaseAdmin";
 import Event from "@/struct/event";
 import axios from "axios";
+import { ExecuteWebhookOptions, Guild } from "oceanic.js";
 
 interface AutoroleData {
     guildId: string;
@@ -14,7 +15,27 @@ async function getData(id: string): Promise<AutoroleData | undefined> {
         .catch(() => undefined);
 }
 
+interface WelcomeDoc {
+    messages: {
+        webhook: {
+            id: string;
+            token: string;
+        },
+        flags: number;
+        content: string;
+        embeds: any[];
+        components: any[];
+    }[];
+};
+
+async function getDoc(id: string): Promise<WelcomeDoc | undefined>{
+    return await axios.get(`${process.env.WEBSITE}/api/guild/${id}/welcome`)
+        .then((res) => res.data)
+        .catch(() => undefined);
+}
+
 export default new Event("on", "guildMemberAdd", async (app, member) => {
+    const guild = app.guilds.get(member.guildID);
     const data = await getData(member.guildID)
 
     if(data){
@@ -27,6 +48,32 @@ export default new Event("on", "guildMemberAdd", async (app, member) => {
         if(data.apps && data.apps[0] && member.bot){
             for(const roleID of data.apps){
                 await member.addRole(roleID).catch(console.log);
+            }
+        }
+    }
+
+    const doc = await getDoc(member.guildID);
+
+    if(doc && doc.messages && doc.messages[0]){
+        for(const message of doc.messages){
+            const webhook = await app.rest.webhooks.get(message.webhook.id, message.webhook.token)
+                .catch(() => undefined);
+
+            if(webhook){
+                const obj: ExecuteWebhookOptions = {
+                    flags: message.flags,
+                    username: `${guild?.name}`,
+                    avatarURL: guild?.iconURL() || undefined,
+                };
+
+                await webhook.execute(message.flags === 0 ? ({
+                    ...obj,
+                    embeds: message.embeds,
+                    content: message.content,
+                }) : ({
+                    ...obj,
+                    components: message.components,
+                }));
             }
         }
     }
