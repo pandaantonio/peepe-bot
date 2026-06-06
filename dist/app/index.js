dfb04a82798e3bed86fc3aa126633ddca330ef5d
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-06-06 00:56:22 -0300

    ok

--- dist/app/index.js
+++ dist/app/index.js
@@ -16,8 +16,7 @@ class App extends oceanic_js_1.Client {
             gateway: {
                 intents: [
                     "GUILDS",
-                    "MESSAGE_CONTENT",
-                    "GUILD_MESSAGES",
+                    "GUILD_MEMBERS"
                 ],
                 autoReconnect: true,
             },
