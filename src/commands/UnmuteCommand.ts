54ec6ee156931d8ca2068a59ba80358c1036e7ac
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-06-09 11:23:32 -0300

    ok

--- src/commands/UnmuteCommand.ts
+++ src/commands/UnmuteCommand.ts
@@ -1,9 +1,9 @@
 import Command from "@/struct/command";
-import timeoutUtil from "@/utils/timeoutUtil";
 import { MessageFlags } from "oceanic.js";
 
 export default new Command()
-    .addName("mute add")
+    .addName("mute remove")
+
     .setRun(async ({ app, guild, interaction }): Promise<void> => {
         if (!guild) return;
 
@@ -14,31 +14,6 @@ export default new Command()
         const user3 = interaction.data.options.getUser("user3", false);
 
         const users = [user1, user2, user3].filter((u) => u !== undefined);
-
-        const time = interaction.data.options.getString("time", true);
-
-        const duration = timeoutUtil(time);
-
-        if (!duration) {
-            await interaction.createFollowup({
-                content:
-                    "❌ Tempo inválido! Exemplos: `5s`, `10m`, `1h`, `7d`, `1h30m`",
-            });
-
-            return;
-        }
-
-        const maxTimeout = 28 * 24 * 60 * 60 * 1000;
-
-        if (duration > maxTimeout) {
-            await interaction.createFollowup({
-                content: "❌ O timeout máximo permitido é de **28 dias**.",
-            });
-
-            return;
-        }
-
-        const timeoutUntil = new Date(Date.now() + duration).toISOString();
 
         for (const user of users) {
             try {
@@ -52,15 +27,15 @@ export default new Command()
                 }
 
                 await member.edit({
-                    communicationDisabledUntil: timeoutUntil,
+                    communicationDisabledUntil: null,
                 });
 
                 res.push(
-                    `${await app.getMenoji("yes")} \`\`${user.globalName ?? user.username}\`\` mutado com sucesso!`
+                    `${await app.getMenoji("yes")} \`\`${user.globalName ?? user.username}\`\` desmutado com sucesso!`
                 );
             } catch {
                 res.push(
-                    `${await app.getMenoji("no")} Erro ao mutar \`\`${user.globalName ?? user.username}\`\``
+                    `${await app.getMenoji("no")} Erro ao desmutar \`\`${user.globalName ?? user.username}\`\``
                 );
             }
         }
@@ -73,7 +48,7 @@ export default new Command()
                     components: [
                         {
                             type: 10,
-                            content: `# 📄 Console de mute\n\n${res
+                            content: `# 📄 Console de desmute\n\n${res
                                 .map((r) => `- ${r}`)
                                 .join("\n")}`,
                         },
