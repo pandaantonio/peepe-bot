a2816f6b63fd1cdab25763cfddcfe630e23e9649
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-06-12 00:41:46 -0300

    ok

--- src/commands/decode/decode.ts
+++ src/commands/decode/decode.ts
@@ -8,21 +8,33 @@ export default new Command().setCommand({
     nameLocalizations: {
         "pt-BR": "decodificar"
     },
-    description: "Null",
+    description: "Decode a text from different formats.",
+    descriptionLocalizations: {
+        "pt-BR": "Decodifique um texto de diferentes formatos."
+    },
     options: [{
         type: 1,
         name: "base64",
-        description: "null",
+        description: "Decode a Base64 string back to text.",
+        descriptionLocalizations: {
+            "pt-BR": "Decodifica um texto em Base64 para texto normal."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }, {
         type: 1,
         name: "morse",
-        description: "null",
+        description: "Decode Morse code back to text.",
+        descriptionLocalizations: {
+            "pt-BR": "Decodifica um código Morse para texto normal."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }, {
         type: 1,
         name: "binary",
-        description: "null",
+        description: "Decode binary code back to text.",
+        descriptionLocalizations: {
+            "pt-BR": "Decodifica um código binário para texto normal."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }],
 });
--- src/commands/encode/encode.ts
+++ src/commands/encode/encode.ts
@@ -8,21 +8,33 @@ export default new Command().setCommand({
     nameLocalizations: {
         "pt-BR": "codificar"
     },
-    description: "Null",
+    description: "Encode a text into different formats.",
+    descriptionLocalizations: {
+        "pt-BR": "Codifique um texto para diferentes formatos."
+    },
     options: [{
         type: 1,
         name: "base64",
-        description: "null",
+        description: "Encode a text to Base64.",
+        descriptionLocalizations: {
+            "pt-BR": "Codifica um texto em Base64."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }, {
         type: 1,
         name: "morse",
-        description: "null",
+        description: "Encode a text to Morse code.",
+        descriptionLocalizations: {
+            "pt-BR": "Codifica um texto em código Morse."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }, {
         type: 1,
         name: "binary",
-        description: "null",
+        description: "Encode a text to binary code.",
+        descriptionLocalizations: {
+            "pt-BR": "Codifica um texto em código binário."
+        },
         options: [TextOption(true), EphemeralOption(false)],
     }],
 });
