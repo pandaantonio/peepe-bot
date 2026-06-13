a4adbbd8faffeda0aeb1f19c8ceb9bdff36f2c42
pandaantonio <pedro.antoniominecraft01@gmail.com>
2026-06-13 14:22:45 -0300

    ok

--- src/commands/utils/encode/encode-base64.ts
+++ src/commands/utils/encode/encode-base64.ts
@@ -1,4 +1,5 @@
 import Command from "@/struct/command";
+import { MessageFlags } from "oceanic.js";
 
 export default new Command()
     .addName("encode base64")
@@ -7,6 +8,10 @@ export default new Command()
         const text = interaction.data.options.getString("text", true);
 
         interaction.createFollowup({
-            content: `\`\`\`${Buffer.from(text).toString('base64')}\`\`\``
+            flags: MessageFlags.IS_COMPONENTS_V2,
+            components: [{
+                type: 10,
+                content: `\`\`\`${Buffer.from(text).toString('base64')}\`\`\``,
+            }]
         });
     })
--- src/commands/utils/encode/encode-binary.ts
+++ src/commands/utils/encode/encode-binary.ts
@@ -1,12 +1,17 @@
 import Command from "@/struct/command";
+import { MessageFlags } from "oceanic.js";
 
 export default new Command()
     .addName("encode binary")
 
-    .setRun(async function({ interaction }){
+    .setRun(async function ({ interaction }) {
         const text = interaction.data.options.getString("text", true);
 
         interaction.createFollowup({
-            content: `\`\`\`${text.split('').map(char => char.charCodeAt(0).toString(2).padStart(8, '0')).join(' ')}\`\`\``
+            flags: MessageFlags.IS_COMPONENTS_V2,
+            components: [{
+                type: 10,
+                content: `\`\`\`${text.split('').map(char => char.charCodeAt(0).toString(2).padStart(8, '0')).join(' ')}\`\`\``,
+            }],
         });
     })
